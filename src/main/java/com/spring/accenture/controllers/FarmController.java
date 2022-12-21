f5c52fa04ad4f915d368acfe21a313202654f5cc
Lopez <luis.pedro.lopez@accenture.com>
2022-12-21 19:16:20 -0300

    Spring 3.0.0 upgrade

--- src/main/java/com/spring/accenture/controllers/FarmController.java
+++ src/main/java/com/spring/accenture/controllers/FarmController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.accenture.entities.Chicken;
@@ -17,7 +16,6 @@ import com.spring.accenture.service.FarmService;
 
 
 @Controller
-@RequestMapping("")
 public class FarmController {
 	
 	@Autowired
@@ -38,7 +36,7 @@ public class FarmController {
     
     //mapeo el menu principal de la granja seleccionada por el usuario, donde puede ver su billetera, acceder a sus datos y al servicio del mercado
     //HTML:farm
-    @GetMapping(value="/farm")
+    @GetMapping(value="/farm/")
     public String farmView(@RequestParam String farmID, Model model) {
     	
     	     
--- src/main/java/com/spring/accenture/repositories/ChickenRepository.java
+++ src/main/java/com/spring/accenture/repositories/ChickenRepository.java
@@ -2,12 +2,12 @@ package com.spring.accenture.repositories;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.spring.accenture.entities.Chicken;
 @Repository
-public interface ChickenRepository extends JpaRepository<Chicken, Long> {
+public interface ChickenRepository extends CrudRepository<Chicken, Long> {
 
 	List<Chicken> findAllByFarmID(long farmID);
 	
